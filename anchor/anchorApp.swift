d17370d3d1dfd547c5016961e4247996525d7162
iediot <eduardnicolae092@gmail.com>
2026-09-09 22:10:28 +0300

    Initial Commit

---
+++ anchor/anchorApp.swift
@@ -0,0 +1,17 @@
+//
+//  anchorApp.swift
+//  anchor
+//
+//  Created by Nicolae-Eduard Tabirca on 09/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct anchorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
